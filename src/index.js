1809e160742c5b7dd59d808741df4893e9d5a97a
mvcbotelho <marcus.itec@gmail.com>
2018-02-14 09:18:58 -0300

    Aula 10 - Curso GoNative

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
+import Todo from './components/Todo'
 
 export default class App extends Component {
+  state = {
+    user: 'Marcus',
+    todos: [
+         {id: 0, text: 'Fazr café' },
+         {id: 1, text: 'Estudar GoNative' },
+    ]
+  };
+
+  addTodo = () => {
+    this.setState({ 
+      todos: [
+        ...this.state.todos,
+        {id: Math.random(), text: 'Novo todo'}
+      ] 
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        
+      <Text>{ this.state.user }</Text>
+        { this.state.todos.map(todos => <Todo key={todos.id} title={todos.text} />) }
+        <Button title="Adiciona todo" onPress={this.addTodo}/>
       </View>
     );
   }
